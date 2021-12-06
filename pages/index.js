0854426bc7918d2f5fac4cac376876f2051cfdea
HenriqueLopes42 <henrique00henk@gmail.com>
2021-12-06 18:57:59 -0400

    up one

---
+++ pages/index.js
@@ -0,0 +1,52 @@
+import Head from 'next/head'
+import {  
+          Text,
+          useColorMode,
+          Heading,
+          Flex,
+          Stack
+        } from '@chakra-ui/react'
+
+import Container from '../components/Container'
+
+export default function Home() {
+
+  const {ColorMode} = useColorMode();
+  const colorSecondary = {
+    light: 'gray.700',
+    dark: 'gray.400'
+  }
+  
+  return (
+    <Container>
+      <Head>
+          <title>Home- Henrique Lopes</title>
+      </Head>
+        <Stack
+        as="main"
+        spacing={8}
+        justifyContent="center"
+        alignItems="flex-start"
+        m="0 auto 4rem auto"
+        maxWidth="700px"
+        px={2}
+        
+        >
+          <Flex
+          flexDirection="column"
+          justifyContent="flex-start"
+          alignItems="flex-start"
+          maxWidth="700px"
+
+          
+          >
+
+            <Heading>
+              Hi, I'm Henrique Lopes
+            </Heading>
+            <Text color={colorSecondary[ColorMode]}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ante nunc, finibus sit amet purus quis, congue vulputate ipsum. Phasellus lobortis bibendum orci, quis imperdiet lectus imperdiet porttitor.</Text>
+          </Flex>
+        </Stack>
+    </Container>
+  )
+}
---
+++ styles/theme.js
@@ -0,0 +1,40 @@
+import { extendTheme } from '@chakra-ui/react'
+import { theme as chakraTheme} from '@chakra-ui/react'
+import { createBreakpoints } from "@chakra-ui/theme-tools"
+
+const fonts = {
+    ...chakraTheme.fonts,
+    body: `Inter,-apple-system,BlinkMacSystem,"Segoe UI",Helvetica,Arieal,sans-serif`,
+    heading: `Inter,-apple-system,BlinkMacSystem,"Segoe UI",Helvetica,Arieal,sans-serif`,
+
+}
+
+const breakpoints = createBreakpoints({
+    sm: "40em",
+    md: "52em",
+    lg: "64em",
+    lg: "62em",
+})
+
+const overrides = {
+    ...chakraTheme,
+    fonts,
+    breakpoints,
+    fontWeights:{
+        normal: 300,
+        medium:600,
+        bold: 700,
+    },
+    fontSizes:{
+        sm: "14px",
+        md: "16px",
+        lg: "18px",
+        xl: "20px",
+        
+    }
+
+}
+
+const customTheme = extendTheme({ overrides })
+
+export default customTheme
